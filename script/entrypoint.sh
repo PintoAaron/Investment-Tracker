11c84fce03bd48917557faa6c48fe7c07649dcff
macquena <aaronpinto111@gmail.com>
2025-03-23 13:47:01 +0000

    udpdate app settings

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -7,4 +7,6 @@ python manage.py migrate
 python manage.py collectstatic --noinput
 
 # Start Gunicorn for production
-gunicorn investmenttracker.wsgi:application --bind 0.0.0.0:8000
+# gunicorn investmenttracker.wsgi:application --bind 0.0.0.0:8000
+
+python manage.py runserver 0.0.0.0:8000
